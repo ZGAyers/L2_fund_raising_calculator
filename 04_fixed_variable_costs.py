8462df4193874c7f222c8be6b36fbac351a28501
Zarah70879 <51766656+Zarah70879@users.noreply.github.com>
2020-05-21 09:47:46 +1200

    Unfinished -Component 04 moved and new component 04 made
    The previous component 04 has been renamed component 05 as an added step has to be made. The new component 04 is for the user to choose if the product has a fixed cost or a variable - unfinished

---
+++ 04_fixed_variable_costs.py
@@ -0,0 +1,87 @@
+# Component 04 - Give the user the option to enter both fixed and variable costs
+
+
+# Not Blank function goes here
+def not_blank(question, error_msg, ):
+    error = error_msg
+
+    valid = False
+    while not valid:
+        response = input(question)
+
+        # If response is blank, question is repeated (loop starts over)
+        if response == "":
+            print(error)
+            continue
+
+        # if response is not blank, program continues
+        else:
+            return response
+
+
+# Number Checking Function goes here
+def num_check(question):
+
+    error = "Please enter a number that is more than zero"
+    valid = False
+    while not valid:
+        try:
+            response = float(input(question))
+
+            if response <= 0:
+
+                print(error)
+            else:
+                return response
+        except ValueError:
+
+            print(error)
+
+# main routine
+
+
+# set up empty list
+printout = []
+total_list = []
+
+# start of loop
+stop = ""
+while stop != "xxx":
+    get_product = not_blank("Product: ",
+                            "Please fill in this field or type 'xxx' to quit")
+
+    if get_product == "xxx":
+        break
+
+    fixed = not_blank("Does this product have a fixed cost? ",
+                      "Please enter <yes> or <not>").lower()
+    if fixed == "y" or "yes":
+        get_cost = num_check("Cost: $")
+
+    elif fixed == "n" or "no":
+        get_cost = num_check("Cost: $")
+
+        get_amount = num_check("Amount: ")
+
+    else:
+        print("Please enter <yes> or <no>")
+        continue
+
+    printout.append("{:.0f} {}, ${:.2f} ".format(get_amount, get_product, get_cost))
+
+    if get_cost > 0:
+        total = get_cost * get_amount
+    else:
+        continue
+
+    total_list.append(total)
+
+total_cost = sum(total_list)
+
+print()
+print("Amount , Item, Cost")
+for item in printout:
+    print(item)
+print()
+print("Total Cost: ")
+print("${:.2f}".format(total_cost))
--- 05_find_price.py
+++ 05_find_price.py
@@ -10,7 +10,7 @@ def not_blank(question, error_msg, ):
     valid = False
     while not valid:
         response = input(question)
-        
+
         # If response is blank, question is repeated (loop starts over)
         if response == "":
             print(error)
